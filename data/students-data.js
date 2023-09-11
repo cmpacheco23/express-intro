ed18d1f31efb5baa519ca57e1debb14d69ccdfac
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-11 17:48:14 -0400

    complete intro express lab

---
+++ data/students-data.js
@@ -0,0 +1,11 @@
+const students = [
+  {studentName: 'Taylor Swift', age: 33, absent: true, assignmentsCompleted: 6, cohortDate: 'Aug 21', location: 'Tennessee' },
+  {studentName: 'Brad Pitt', age: 59, absent: false, assignmentsCompleted: 8, cohortDate: 'Aug 21', location: 'California' },
+  {studentName: 'Emily Osment', age: 31, absent: false, assignmentsCompleted: 7, cohortDate: 'Aug 21', location: 'California' },
+  {studentName: 'Jennifer Lopez', age: 54, absent: true, assignmentsCompleted: 3, cohortDate: 'Aug 21', location: 'California' },
+  {studentName: 'Pitbull Mr.Worldwide', age: 42, absent: false, assignmentsCompleted: 9, cohortDate: 'Aug 21', location: 'Florida' },
+]
+
+export {
+  students
+}
